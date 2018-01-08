54e493f6cacd4ed1c6f3171a96344aee2fc8de2c
Josh Rochon and Qianqian Hu <qianqian@>
2018-01-08 13:45:49 -0800

    create calculator logic for subtraction, division and multiplication

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-var calculator = function(){
+//Addition
+
+var addCalc = function(){
 
 var num1 = parseInt(prompt("Enter a number"));
 var num2 = parseInt(prompt("Enter another number"))
@@ -13,4 +15,61 @@ alert("Great, the sum of those numbers is " + add(num1, num2) + ".");
 
 }
 
-calculator();
+addCalc();
+
+//Subtraction
+
+var subCalc = function(){
+
+var num1 = parseInt(prompt("Enter a number"));
+var num2 = parseInt(prompt("Enter another number"))
+
+var subtract = function(){
+
+	return(num1 - num2);
+}
+
+
+alert("Great, the difference of those numbers is " + subtract(num1, num2) + ".");
+
+}
+
+subCalc();
+
+//Multiplication
+
+var multiCalc = function(){
+
+var num1 = parseInt(prompt("Enter a number"));
+var num2 = parseInt(prompt("Enter another number"))
+
+var multiply = function(){
+
+	return(num1 * num2);
+}
+
+
+alert("Great, the result of those numbers is " + multiply(num1, num2) + ".");
+
+}
+
+multiCalc();
+
+//Division
+
+var divCalc = function(){
+
+var num1 = parseInt(prompt("Enter a number"));
+var num2 = parseInt(prompt("Enter another number"))
+
+var divide = function(){
+
+	return(num1 / num2);
+}
+
+
+alert("Great, the result of those numbers is " + divide(num1, num2) + ".");
+
+}
+
+divCalc();
